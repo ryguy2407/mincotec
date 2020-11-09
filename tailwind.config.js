15c39c71d85b95e60724b79bc2fc2806a374190b
Ryan Murray <ryanmurrayemail@gmail.com>
2020-11-09 11:24:31 +1000

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,13 +15,28 @@ module.exports = {
     fontFamily: {
       'sans': ['"Neutrif Studio Regular"', 'Helvetica', 'Arial', 'sans-serif'],
     },
-    extend: {},
+    extend: {
+      backgroundOpacity: {
+        '10': '0.1',
+        '20': '0.2',
+        '30': '0.3',
+        '40': '0.4',
+        '50': '0.5',
+        '60': '0.6',
+        '70': '0.7',
+        '80': '0.8',
+        '90': '0.9',
+        '100': '1',
+}
+    },
   },
   variants: {
     display: ['responsive', 'hover', 'focus', 'group-hover'],
     opacity: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
     visibility: ['responsive', 'hover', 'focus', 'group-hover'],
     transitionProperty: ['responsive', 'hover', 'focus'],
+    cursor: ['responsive', 'hover', 'focus'],
+    padding: ['responsive', 'hover', 'focus'],
   },
   plugins: [
     require('@tailwindcss/ui'),
